f61719a5e6498540c22ac2127be6d2138309ebe9
Amierul Hakeem <amierulhakeem02@gmail.com>
2024-02-08 00:00:42 +0000

    test after

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 DigitalOut myled(LED1);
 
 int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+    while(1) {
+        myled = 1;
+        wait(0.2);
+        myled = 0;
+        wait(0.2);
+    }
 }
 
 first code
